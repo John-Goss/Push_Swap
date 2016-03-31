b981d589be2e21b61d529d3bd875c3160d3c9363
Jonathan LE QUERE <jle-quer@e3r9p9.42.fr>
2016-03-31 13:53:29 +0200

    Corec do.

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jle-quer <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/14 15:29:37 by jle-quer          #+#    #+#             */
-/*   Updated: 2016/03/24 17:55:24 by jle-quer         ###   ########.fr       */
+/*   Updated: 2016/03/31 13:39:04 by jle-quer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,27 @@
 
 int	main(int ac, char **av)
 {
+	int		i;
+
 	g_opt_r = 0;
 	g_opt_c = 0;
 	g_opt_v = 0;
-	if (ac < 2)
-		return (parse_error(2));
+	i = 0;
+	if (ac < 3)
+	{
+		if (ac == 1 || ac < 1)
+			return (0);
+		else
+		{
+			if (av[1][i] == '-')
+				i++;
+			while (av[1][i])
+			{
+				if (ft_isdigit(av[1][i++]) == 0)
+					return (0);
+			}
+		}
+	}
 	parse_opt(ac, av);
 	return (0);
 }
--- parse_opt.c
+++ parse_opt.c
@@ -6,7 +6,7 @@
 /*   By: jle-quer <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/21 12:23:57 by jle-quer          #+#    #+#             */
-/*   Updated: 2016/03/25 12:23:17 by jle-quer         ###   ########.fr       */
+/*   Updated: 2016/03/31 13:52:06 by jle-quer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@ static int	set_opt(char *av)
 		return (-1);
 	while (av[i])
 	{
-		if (ft_isdigit(av[i]) == 1 && (av[i - 1] != '-' || av[i + 1]))
-			return (-1);
-		else if (ft_isdigit(av[i]) == 1)
+		//if (ft_isdigit(av[i]) == 1 && (av[i - 1] != '-' || av[i + 1]))
+		//	return (0);
+		if (ft_isdigit(av[i]) == 1)
 			return (0);
 		else if (av[i] == 'v' && g_opt_v == 0)
 			g_opt_v += 1;
